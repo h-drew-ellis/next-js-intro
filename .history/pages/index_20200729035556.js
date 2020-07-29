99833e4eaa4497afd9b303b82da6d830d64d0eaf
Andrew Ellis <h.drew.ellis@gmail.com>
2020-07-29 05:33:51 -0500

    completed rendering moving to dynamic routes

--- .history/pages/index_20200729035556.js
+++ .history/pages/index_20200729035556.js
@@ -1,18 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home ({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -26,20 +16,6 @@ export default function Home ({ allPostsData }) {
            You can learn this build from {' '}
           <a href="https://nextjs.org/learn">Vercel</a>.
         </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-       <h2 className={utilStyles.headingLg}>Blog</h2>
-       <ul className={utilStyles.list}>
-         {allPostsData.map(({ id, date, title}) => (
-           <li className={utilStyles.listItem} key={id}>
-             {title}
-             <br />
-             {id}
-             <br />
-             {date}
-           </li>
-         ))}
-       </ul>
       </section>
     </Layout>
   )
